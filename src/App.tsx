728e7c7c7c00425c50a8640e67e56d5c99c5b729
thodex <abdallhameed2005@gmail.com>
2025-05-14 22:27:43 +0300

    starter

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import BodyMapFront from "./components/BodyMapFront";
+import BodyMapBack from "./components/BodyMapBack";
+function App() {
+  return (
+    <>
+      <BodyMapFront />
+      <BodyMapBack />
+    </>
+  );
+}
+
+export default App;
