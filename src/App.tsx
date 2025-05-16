9036c3ed0141431dece56760eac15f8e36190df9
thodex <abdallhameed2005@gmail.com>
2025-05-16 20:06:04 +0300

    feat:add yup and formik for search bar

--- src/App.tsx
+++ src/App.tsx
@@ -68,7 +68,7 @@ function App() {
 
       {isModalOpen && <Modal onClose={() => setIsModalOpen(false)} />}
 
-      <div className="flex flex-row justify-between items-start w-full max-md:flex-col-reverse max-md:items-center max-md:justify-center">
+      <div className="flex flex-row justify-between items-start w-full max-md:flex-col-reverse max-md:items-center max-md:justify-center gap-4">
         <div className="flex flex-row justify-center max-md:flex-col  items-start max-md:items-center w-1/2 ">
           <BodyMapFront
             onClick={(event) => {
--- src/components/SeachInput.tsx
+++ src/components/SeachInput.tsx
@@ -1,9 +1,24 @@
 import { useState, useEffect } from "react";
 import type { SearchInputProps } from "../types/SearchInputProps";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 function SeachInput({ setQuery }: SearchInputProps) {
   const [placeholderText, setPlaceholderText] = useState<string>("");
   const [timeoutId, setTimeoutId] = useState<number>();
+
+  const formik = useFormik({
+    initialValues: {
+      Query: "",
+    },
+    onSubmit: () => {},
+    validationSchema: Yup.object({
+      Query: Yup.string().required(
+        "Please write or select a body part or muscle"
+      ),
+    }),
+    validateOnMount: true,
+  });
 
   useEffect(() => {
     const text = "Search exercises";
@@ -21,6 +36,7 @@ function SeachInput({ setQuery }: SearchInputProps) {
   }, []);
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    formik.handleChange(event);
     //Debouncing
     if (timeoutId) clearTimeout(timeoutId);
 
@@ -34,12 +50,19 @@ function SeachInput({ setQuery }: SearchInputProps) {
   };
 
   return (
-    <input
-      type="text"
-      className="outline-none border-2 p-2 w-1/2 text-white font-bold border-white rounded-2xl text-center"
-      placeholder={placeholderText}
-      onChange={handleChange}
-    />
+    <>
+      <input
+        name="Query"
+        type="text"
+        value={formik.values.Query}
+        className="outline-none border-2 p-2 w-1/2 text-white font-bold border-white rounded-2xl text-center"
+        placeholder={placeholderText}
+        onChange={handleChange}
+      />
+      {formik.errors.Query && formik.touched.Query && (
+        <p className="text-red-500 text-sm mt-1">{formik.errors.Query}</p>
+      )}
+    </>
   );
 }
 
